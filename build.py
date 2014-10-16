d5d59bef261a82f2a4153b5c3196cf288c465984
Stefano Zaghi <stefano.zaghi@gmail.com>
2014-10-16 09:46:01 +0200

    Remove re module from build deps

--- build.py
+++ build.py
@@ -24,7 +24,7 @@ def initialize(project):
   project.version = version
   project.build_depends_on('coverage')
   project.build_depends_on('pylint')
-  project.build_depends_on('re')
+  #project.build_depends_on('re')
   project.build_depends_on('unittest')
   project.depends_on('markdown')
   project.depends_on('yattag')
