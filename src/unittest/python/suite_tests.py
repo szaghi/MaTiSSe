9703edea2acc5396f901c1c950939862abfb5519
Stefano Zaghi <stefano.zaghi@gmail.com>
2014-10-16 11:46:51 +0200

    Fix maxDiff bug in unittest suite

--- src/unittest/python/suite_tests.py
+++ src/unittest/python/suite_tests.py
@@ -2,13 +2,12 @@
 """Testing columns environment"""
 import unittest
 from matisse.presentation.presentation import Presentation
-#from ..presentation.presentation import Presentation
 
-class ColumnsTest(unittest.TestCase):
+class SuiteTest(unittest.TestCase):
   """Testing MaTiSSe.py Presentation with columns environment."""
-
   def test_slides(self):
     """Testing Slide class instances."""
+    self.maxDiff = None
     source = open('src/unittest/python/slides/test.md').read()
     talk = Presentation(source=source)
     self.assertEqual(open('src/unittest/python/slides/test/index.html').read(),talk.to_html())
@@ -16,6 +15,7 @@ class ColumnsTest(unittest.TestCase):
 
   def test_subsections(self):
     """Testing Subsection class instances."""
+    self.maxDiff = None
     source = open('src/unittest/python/subsections/test.md').read()
     talk = Presentation(source=source)
     self.assertEqual(open('src/unittest/python/subsections/test/index.html').read(),talk.to_html())
@@ -23,6 +23,7 @@ class ColumnsTest(unittest.TestCase):
 
   def test_sections(self):
     """Testing Section class instances."""
+    self.maxDiff = None
     source = open('src/unittest/python/sections/test.md').read()
     talk = Presentation(source=source)
     self.assertEqual(open('src/unittest/python/sections/test/index.html').read(),talk.to_html())
@@ -30,6 +31,7 @@ class ColumnsTest(unittest.TestCase):
 
   def test_boxes(self):
     """Testing Box class instances."""
+    self.maxDiff = None
     source = open('src/unittest/python/boxes/test.md').read()
     talk = Presentation(source=source)
     self.assertEqual(open('src/unittest/python/boxes/test/index.html').read(),talk.to_html())
@@ -37,6 +39,7 @@ class ColumnsTest(unittest.TestCase):
 
   def test_figures(self):
     """Testing Figure class instances."""
+    self.maxDiff = None
     source = open('src/unittest/python/figures/test.md').read()
     talk = Presentation(source=source)
     self.assertEqual(open('src/unittest/python/figures/test/index.html').read(),talk.to_html())
@@ -44,6 +47,7 @@ class ColumnsTest(unittest.TestCase):
 
   def test_notes(self):
     """Testing Note class instances."""
+    self.maxDiff = None
     source = open('src/unittest/python/notes/test.md').read()
     talk = Presentation(source=source)
     self.assertEqual(open('src/unittest/python/notes/test/index.html').read(),talk.to_html())
@@ -51,6 +55,7 @@ class ColumnsTest(unittest.TestCase):
 
   def test_columns(self):
     """Testing Columns class instances."""
+    self.maxDiff = None
     source = open('src/unittest/python/columns/test.md').read()
     talk = Presentation(source=source)
     self.assertEqual(open('src/unittest/python/columns/test/index.html').read(),talk.to_html())
@@ -58,6 +63,7 @@ class ColumnsTest(unittest.TestCase):
 
   def test_columns_figures(self):
     """Testing Columns with Figures."""
+    self.maxDiff = None
     source = open('src/unittest/python/columns_and_figures/test.md').read()
     talk = Presentation(source=source)
     self.assertEqual(open('src/unittest/python/columns_and_figures/test/index.html').read(),talk.to_html())
@@ -65,6 +71,7 @@ class ColumnsTest(unittest.TestCase):
 
   def test_columns_figures_notes(self):
     """Testing Columns with Figures and Notes."""
+    self.maxDiff = None
     source = open('src/unittest/python/columns_and_figures_and_notes/test.md').read()
     talk = Presentation(source=source)
     self.assertEqual(open('src/unittest/python/columns_and_figures_and_notes/test/index.html').read(),talk.to_html())
